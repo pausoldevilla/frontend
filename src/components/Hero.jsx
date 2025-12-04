d5f5ea67f6ca831e1a5bc29598287b401943f47c
pau <pausoldevilla@gmail.com>
2025-12-04 10:10:17 +0100

    mensaje bienvenida

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,18 +10,18 @@ export default function Hero() {
         backgroundPosition: "center",
       }}
     >
-      <div className="absolute inset-0 bg-gradient-to-r from-black/60 to-transparent z-0"></div>
-
-      <div className="hero-content relative z-10 max-w-screen-md pt-40">
-        <h2 className="text-5xl mb-4 font-bold drop-shadow-lg">
+      <div className="hero-content relative z-10 max-w-screen-md pt-24 lg:pt-40 space-y-4 bg-black/90 p-4">
+        <h2 className="text-5xl font-bold">
           Bienvenido
         </h2>
-        <p className="text-xl mb-6 drop-shadow-md">
+
+        <p className="text-xl">
           Descubre las mejores plantas para tu hogar y jardín
         </p>
+
         <a
           href="#categories"
-          className="font-bold border-b border-white hover:text-green-900 transition-colors drop-shadow-sm"
+          className="font-bold border-b border-white hover:text-green-900 transition-colors"
         >
           Explorar Categorías
         </a>
